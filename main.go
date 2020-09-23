f5d0ace877e68e9018877bb13e4e900f64727a60
Sebastian Reverso <sebastian.reverso@sovos.com>
2020-09-23 12:46:31 -0300

    Basic data types examples

--- main.go
+++ main.go
@@ -5,5 +5,22 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello from a module, world!")
+	var i int
+	i = 45
+	fmt.Println(i)
+
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	firstName := "Arthur"
+	fmt.Println(firstName)
+
+	b := true
+	fmt.Println(b)
+
+	c := complex(3, 4)
+	fmt.Println(c)
+
+	r, im := real(c), imag(c)
+	fmt.Println(r, im)
 }
