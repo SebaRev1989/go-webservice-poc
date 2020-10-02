044ecb1bb54ff973c10c07d67abb665931ab774e
Sebastian Reverso <sebastian.reverso@sovos.com>
2020-10-02 17:04:49 -0300

    Array examples

--- main.go
+++ main.go
@@ -2,16 +2,15 @@ package main
 
 import "fmt"
 
-const (
-	first = iota
-	second
-)
-
-const (
-	third = iota
-	fourth
-)
-
 func main() {
-	fmt.Println(first, second, third, fourth)
+	var arr [3]int
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+
+	fmt.Println(arr)
+	fmt.Println(arr[1])
+
+	newArr := [3]int{3, 4, 5}
+	fmt.Println(newArr)
 }
